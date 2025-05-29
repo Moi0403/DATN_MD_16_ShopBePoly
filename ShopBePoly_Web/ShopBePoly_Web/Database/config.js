fd223efe71151266057223837565ca6e6da2c673
nguyenchien2408 <vts273593@gmail.com>
2025-05-29 23:36:55 +0700

    Api comment

--- ShopBePoly_Web/ShopBePoly_Web/Database/config.js
+++
@@ -1,4 +0,0 @@
-    const config = {
-        host: '10.11.11.55',
-        port: '3000'
-    };
--- ShopBePoly_Web/server.js
+++ ShopBePoly_Web/server.js
@@ -15,7 +15,11 @@ const productModel = require('./Database/productModel');
 const COMOMJS = require('./Database/COMOM');
 const userModel = require('./Database/userModel');
 const cartModel = require('./Database/cartModel');
+<<<<<<< HEAD
+const commentModel = require('./Database/commentModel');
+=======
 const categoryModel = require('./Database/categoryModel');
+>>>>>>> a293b84fbe0e24537d3f45e306b8bf3fbfa94c61
 
 const uri = COMOMJS.uri;
 
@@ -252,6 +256,54 @@ router.get('/list_category',async(req,res)=>{
     res.send(category);
 });
 
+<<<<<<< HEAD
+// ds comment 'http://localhost:3000/api/list_comment'// Lấy giỏ hàng http://localhost:3000/api/:useId
+router.get('/api/cart/:userId', async (req, res) => {
+    try {
+        const cartItems = await cartModel.find({ id_user: req.params.userId });
+        res.json(cartItems);
+    } catch (error) {
+        console.error('Lỗi khi lấy giỏ hàng:', error);
+        res.status(500).json({ error: 'Lỗi khi lấy giỏ hàng' });
+    }
+});
+
+// thêm comment 'http://localhost:3000/api/add_comment'
+router.post('/add_comment', async (req, res)=>{
+    
+    let data = req.body;
+    let kq = await commentModel.create(data);
+
+    if(kq){
+        console.log('Thêm comment thành công');
+        let comment = await commentModel.find();
+        res.send(comment);
+    } else{
+        console.log('Thêm comment không thành công');
+    }
+
+})
+
+// sửa comment 'http://localhost:3000/api/up_comment/ id'
+router.put('/up_comment/:id', async (req, res)=>{
+    try{
+        const id = req.params.id;
+        const data = req.body;
+        
+        const kq = await commentModel.findByIdAndUpdate(id, data, { new: true });
+
+        if(kq){
+            console.log('Sửa thành công');
+            let usr = await commentModel.find();
+            res.send(usr);
+        } else{
+            res.send('Không tìm thấy comment để sửa');
+        }
+    } catch (error){
+        res.send('Lỗi khi sửa')
+    }
+})
+=======
 router.post('/add_category',async(req,res)=>{
 
     let data = req.body;
@@ -304,5 +356,6 @@ router.delete('/del_category/:id',async (req,res)=>{
         
     }
 })
+>>>>>>> a293b84fbe0e24537d3f45e306b8bf3fbfa94c61
 
 app.use(express.json()); // bắt buộc để đọc req.body
