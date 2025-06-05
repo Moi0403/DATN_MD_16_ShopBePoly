2f339b6678b9134a524ad60110e7d78721c38cd9
nguyenchien2408 <vts273593@gmail.com>
2025-06-05 22:09:25 +0700

    App layout

--- ShopBePoly_Web/Web/Style_Sidebar/Sidebar.js
+++
@@ -1,9 +0,0 @@
-function toggleSidebar() {
-  const sidebar = document.getElementById("sidebar");
-  const overlay = document.getElementById("overlay");
-  const body = document.body;
-
-  sidebar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  body.classList.toggle("sidebar-open"); // thêm class để điều chỉnh layout
-}
