0310a452d8012b72fc91231b13a906a7d9735384
Nam11122004 <namden9399@gmail.com>
2025-06-11 00:12:10 +0700

    update api

--- ShopBePoly_Web/Database/config.js
+++ ShopBePoly_Web/Database/config.js
@@ -1,10 +1,6 @@
-    // const config = {
-    //     host: '192.168.1.3',
-    //     port: '3000'
-    // };
-
-        const config = {
-        host: '192.168.55.26',
+    const config = {
+        host: '192.168.1.3',
         port: '3000'
     };
+
 
